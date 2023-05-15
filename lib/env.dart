773521d8ea1986487f76a3e4c7d6cd661cfac216
Léon Logli <leonlogli@gmail.com>
2023-05-15 13:34:20 +0200

    Add http and api call to firebase

---
+++ lib/env.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final kFirebaseDb = dotenv.env['FIREBASE_DB']!;
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:flutter_shopping_list/widgets/grocery_list.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
